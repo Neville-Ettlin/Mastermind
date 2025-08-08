1e3fe357a5dc777baad888d2feccb0ec2ab89aa3
Neville_Ettlin <neville.ettlin@bluewin.ch>
2025-08-08 10:56:57 +0200

    random

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,12 @@ import java.util.Random;
             System.out.println("Viel Glück!!");
 
             /*
-            0 = Rot
-            1 = Grün
-            2 = Blau
-            3 = Orange
-            4 = Gelb
-            5 = Weis
-            6 = Grau
-            7 = Rosa
+            0 = Blau
+            1 = Rot
+            2 = Grün
+            3 = Gelb
+            4 = Orange
+            5 = Violet
              */
 
             //Generation Computer Farben
@@ -27,30 +25,145 @@ import java.util.Random;
             int abc = 0;
             int abcd = 0;
 
-            int farbe1_Com = 0;
-            int farbe2_Com = 0;
-            int farbe3_Com = 0;
-            int farbe4_Com = 0;
+            int farbe1_Com_random = 0;
+            int farbe2_Com_random = 0;
+            int farbe3_Com_random = 0;
+            int farbe4_Com_random = 0;
 
+            String farbe1_Com = "";
+            String farbe2_Com = "";
+            String farbe3_Com = "";
+            String farbe4_Com = "";
 
             for (int i = 0; i <= 4; i++) {
                 Random random = new Random();
-                int randomZahl = random.nextInt(8);
+                int randomZahl = random.nextInt(6);
 
                 if (a == 0) {
-                     farbe1_Com = randomZahl;
+                    farbe1_Com_random = randomZahl;
                     ++a;
                 } else if (ab == 0) {
-                      farbe2_Com = randomZahl;
+                    farbe2_Com_random = randomZahl;
                     ++ab;
                 } else if (abc == 0) {
-                     farbe3_Com = randomZahl;
+                    farbe3_Com_random = randomZahl;
                     ++abc;
                 } else if (abcd == 0) {
-                     farbe4_Com = randomZahl;
+                    farbe4_Com_random = randomZahl;
                     ++abcd;
+                }
+            }
+
+            //Farb zuweisung Farbe1_Com
+                switch (farbe1_Com_random) {
+                    case 0:
+                        farbe1_Com = "Blau";
+                        break;
+                    case 1:
+                        farbe1_Com = "Rot";
+                        break;
+                    case 2:
+                        farbe1_Com = "Grün";
+                        break;
+                    case 3:
+                        farbe1_Com = "Gelb";
+                        break;
+                    case 4:
+                        farbe1_Com = "Orange";
+                        break;
+                    case 5:
+                        farbe1_Com = "Violet";
+                        break;
+
+                    default:
+                        System.out.println("error");
+                        break;
+                }
+
+            //Farb zuweisung Farbe2_Com
+                switch (farbe2_Com_random) {
+                    case 0:
+                        farbe2_Com = "Blau";
+                        break;
+                    case 1:
+                        farbe2_Com = "Rot";
+                        break;
+                    case 2:
+                        farbe2_Com = "Grün";
+                        break;
+                    case 3:
+                        farbe2_Com = "Gelb";
+                        break;
+                    case 4:
+                        farbe2_Com = "Orange";
+                        break;
+                    case 5:
+                        farbe2_Com = "Violet";
+                        break;
+
+                    default:
+                        System.out.println("error");
+                        break;
+                }
+
+            //Farb zuweisung Farbe3_com
+                switch (farbe3_Com_random) {
+                    case 0:
+                        farbe3_Com = "Blau";
+                        break;
+                    case 1:
+                        farbe3_Com = "Rot";
+                        break;
+                    case 2:
+                        farbe3_Com = "Grün";
+                        break;
+                    case 3:
+                        farbe3_Com = "Gelb";
+                        break;
+                    case 4:
+                        farbe3_Com = "Orange";
+                        break;
+                    case 5:
+                        farbe3_Com = "Violet";
+                        break;
+
+                    default:
+                        System.out.println("error");
+                        break;
+                }
+
+            //Farb zuweisung Farbe4_com
+            switch (farbe4_Com_random) {
+                case 0:
+                    farbe4_Com = "Blau";
+                    break;
+                case 1:
+                    farbe4_Com = "Rot";
+                    break;
+                case 2:
+                    farbe4_Com = "Grün";
+                    break;
+                case 3:
+                    farbe4_Com = "Gelb";
+                    break;
+                case 4:
+                    farbe4_Com = "Orange";
+                    break;
+                case 5:
+                    farbe4_Com = "Violet";
+                    break;
+
+                default:
+                    System.out.println("error");
+                    break;
+            }
+
+                System.out.println(farbe2_Com);
+                System.out.println(farbe1_Com);
+                System.out.println(farbe3_Com);
+                System.out.println(farbe4_Com);
+
+
                 }
 
             }
-            }
-        }
