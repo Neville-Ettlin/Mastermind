d0e133c203d02b9eb1f788bfc8a13b6d0207ed25
Damir_Kharakhursakh <damir_kharakhursakh@sluz.ch>
2025-08-08 08:17:26 +0200

    z

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import java.util.Random;
     public class Main {
         public static void main(String[] args) {
-            System.out.println("Hoi Damir");
+            System.out.println("Sie haben das Spiel angefangen");
             //Generiert Random Zahl und definiert 4 Variablen
+
+            System.out.println("Herzlich Willkommen zu Mastermind");
+            System.out.println("Ihre Aufgabe ist die Farben zu erraten. Insgesamt haben Sie 12 Versuche die 4 richtige Farben zu finden.");
+            System.out.println("Die mögliche Farben sind: Rot, Grün, Blau, Orange, Gelb, Weis, Grau und Rosa.");
+            System.out.println("Viel Glück!");
 
             /*
             1 = Rot
