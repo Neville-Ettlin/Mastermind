688d832be4d0e7ef53a53a7e07f2bbe006c7cf4e
Damir_Kharakhursakh <damir_kharakhursakh@sluz.ch>
2025-08-11 09:11:39 +0200

    z

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,10 @@ public class Main {
 
             if (Objects.equals(Antwort, Ja)) {
                 System.out.println("Sie haben das Spiel angefangen");
+                System.out.println("Die mögliche Farben sind: Blau, Rot, Grün, Gelb, Orange, Violet.");
             } else {
                 System.out.println("Ihre Aufgabe ist die Farben zu erraten. Insgesamt haben Sie 12 Versuche die 4 richtige Farben zu finden.");
-                System.out.println("Die mögliche Farben sind: Rot, Grün, Blau, Orange, Gelb, Weis, Grau und Rosa.");
+                System.out.println("Die mögliche Farben sind: Blau, Rot, Grün, Gelb, Orange, Violet.");
                 System.out.println("Viel Glück!!");
             }
 
@@ -179,108 +180,132 @@ public class Main {
                     break;
             }
 
-                System.out.println(farbe1_Com);
-                System.out.println(farbe2_Com);
-                System.out.println(farbe3_Com);
-                System.out.println(farbe4_Com);
 
-            // User gibt seine Varianten an
-
-            Scanner FarbeScanner = new Scanner(System.in);
-            String userValue1 = FarbeScanner.nextLine();
-            String userValue2 = FarbeScanner.nextLine();
-            String userValue3 = FarbeScanner.nextLine();
-            String userValue4 = FarbeScanner.nextLine();
-
-            int FarbeRichtig = 0;
-            int FarbenPosition = 0;
-            int FarbenFalsch = 0;
+            int versuche = 12;
 
 
+            for (int i = 0; i <= 12; i++ ) {
 
-            if (Objects.equals(userValue1, farbe1_Com)) {
-                FarbeRichtig = FarbeRichtig + 1;
-            } else if (Objects.equals(userValue1, farbe2_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue1, farbe3_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue1, farbe4_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else {
-                FarbenFalsch = FarbenFalsch + 1;
+                if (i == 12) {
+                    System.out.println("Die richtigen Farben waren:");
+                    System.out.println(farbe1_Com);
+                    System.out.println(farbe2_Com);
+                    System.out.println(farbe3_Com);
+                    System.out.println(farbe4_Com);
+                    System.exit(1);
+                }
+
+                if (versuche == 1) {
+                    System.out.println("Sie haben noch" + " " + versuche + " " + "Versuch");
+                } else {
+                    System.out.println("Sie haben noch" + " " + versuche + " " + "Versuche");
+                    versuche = versuche - 1;
+                }
+
+                System.out.println("Geben Sie ihre Farben einzeln ein:");
+
+
+                // User gibt seine Varianten an
+
+                Scanner FarbeScanner = new Scanner(System.in);
+                String userValue1 = FarbeScanner.nextLine();
+                String userValue2 = FarbeScanner.nextLine();
+                String userValue3 = FarbeScanner.nextLine();
+                String userValue4 = FarbeScanner.nextLine();
+
+
+                int FarbeRichtig = 0;
+                int FarbenPosition = 0;
+                int FarbenFalsch = 0;
+
+
+                if (Objects.equals(userValue1, farbe1_Com)) {
+                    FarbeRichtig = FarbeRichtig + 1;
+                } else if (Objects.equals(userValue1, farbe2_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue1, farbe3_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue1, farbe4_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else {
+                    FarbenFalsch = FarbenFalsch + 1;
+                }
+
+                if (Objects.equals(userValue2, farbe2_Com)) {
+                    FarbeRichtig = FarbeRichtig + 1;
+                } else if (Objects.equals(userValue2, farbe1_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue2, farbe3_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue2, farbe4_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else {
+                    FarbenFalsch = FarbenFalsch + 1;
+                }
+
+                if (Objects.equals(userValue3, farbe3_Com)) {
+                    FarbeRichtig = FarbeRichtig + 1;
+                } else if (Objects.equals(userValue3, farbe2_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue3, farbe1_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue3, farbe4_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else {
+                    FarbenFalsch = FarbenFalsch + 1;
+                }
+
+                if (Objects.equals(userValue4, farbe4_Com)) {
+                    FarbeRichtig = FarbeRichtig + 1;
+                } else if (Objects.equals(userValue4, farbe2_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue4, farbe3_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else if (Objects.equals(userValue4, farbe1_Com)) {
+                    FarbenPosition = FarbenPosition + 1;
+                } else {
+                    FarbenFalsch = FarbenFalsch + 1;
+                }
+
+                if (FarbeRichtig == 0) {
+                    System.out.println("Alle Farben sind falsch oder haben falsche Position");
+                } else if (FarbeRichtig == 1) {
+                    System.out.println("Eine Farbe ist richtig");
+                } else if (FarbeRichtig == 2) {
+                    System.out.println("Zwei Farben sind richtig");
+                } else if (FarbeRichtig == 3) {
+                    System.out.println("Drei Farben sind richtig");
+                } else {
+                    System.out.println("Du hast gewonnen!");
+                    System.exit(1);
+                }
+
+                if (FarbenFalsch == 0) {
+                    System.out.println("Keine Farben sind falsch");
+                } else if (FarbenFalsch == 1) {
+                    System.out.println("Eine Farbe ist falsch");
+                } else if (FarbenFalsch == 2) {
+                    System.out.println("Zwei Farben sind falsch");
+                } else if (FarbenFalsch == 3) {
+                    System.out.println("Drei Farben sind falsch");
+                } else {
+                    System.out.println("Alle Farben sind falsch");
+                }
+
+                if (FarbenPosition == 0) {
+                    System.out.println("Keine Farben haben falsche Position");
+                } else if (FarbenPosition == 1) {
+                    System.out.println("Eine Farbe hat falsche Position");
+                } else if (FarbenPosition == 2) {
+                    System.out.println("Zwei Farben haben falsche Position");
+                } else if (FarbenPosition == 3) {
+                    System.out.println("Drei Farben haben falsche Position");
+                } else {
+                    System.out.println("Alle Farben haben falsche Position");
+                }
+
             }
 
-            if (Objects.equals(userValue2, farbe1_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue2, farbe2_Com)) {
-                FarbeRichtig = FarbeRichtig + 1;
-            } else if (Objects.equals(userValue2, farbe3_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue2, farbe4_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else {
-                FarbenFalsch = FarbenFalsch + 1;
-            }
-
-            if (Objects.equals(userValue3, farbe1_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue3, farbe2_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue3, farbe3_Com)) {
-                FarbeRichtig = FarbeRichtig + 1;
-            } else if (Objects.equals(userValue3, farbe4_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else {
-                FarbenFalsch = FarbenFalsch + 1;
-            }
-
-            if (Objects.equals(userValue4, farbe1_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue4, farbe2_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue4, farbe3_Com)) {
-                FarbenPosition = FarbenPosition + 1;
-            } else if (Objects.equals(userValue4, farbe4_Com)) {
-                FarbeRichtig = FarbeRichtig + 1;
-            } else {
-                FarbenFalsch = FarbenFalsch + 1;
-            }
-
-            if (FarbeRichtig == 0) {
-                System.out.println("Alle Farben sind Falsch");
-            } else if (FarbeRichtig == 1) {
-                System.out.println("Eine Farbe ist richtig");
-            } else if (FarbeRichtig == 2) {
-                System.out.println("Zwei Farben sind richtig");
-            } else if (FarbeRichtig == 3) {
-                System.out.println("Drei Farben sind richtig");
-            } else {
-                System.out.println("Alle Farben sind richtig");
-            }
-
-            if (FarbenFalsch == 0) {
-                System.out.println("Keine Farben sind falsch");
-            } else if (FarbenFalsch == 1) {
-                System.out.println("Eine Farbe ist falsch");
-            } else if (FarbenFalsch == 2) {
-                System.out.println("Zwei Farben sind falsch");
-            } else if (FarbenFalsch == 3) {
-                System.out.println("Drei Farben sind falsch");
-            } else {
-                System.out.println("Alle Farben sind falsch");
-            }
-
-            if (FarbenPosition == 0) {
-                System.out.println("Keine Farben haben falsche Position");
-            } else if (FarbenPosition == 1) {
-                System.out.println("Eine Farbe hat falsche Position");
-            } else if (FarbenPosition == 2) {
-                System.out.println("Zwei Farben haben falsche Position");
-            } else if (FarbenPosition == 3) {
-                System.out.println("Drei Farben haben falsche Position");
-            } else {
-                System.out.println("Alle Farben haben falsche Position");
-            }
 
 
 
